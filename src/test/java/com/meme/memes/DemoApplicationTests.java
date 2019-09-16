918e73e91c46319be4ca5c9c4576412be7e333ed
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-09-15 20:34:35 -0400

    Add valuable unit test

--- src/test/java/com/meme/memes/DemoApplicationTests.java
+++ src/test/java/com/meme/memes/DemoApplicationTests.java
@@ -1,16 +1,32 @@
 package com.meme.memes;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.isNotNull;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class DemoApplicationTests {
 
+	@Autowired
+	MockMvc mvc;
+
 	@Test
-	public void contextLoads() {
+	public void veryImportantUnitTest() {
+		// importantLogic()
+		// 
+		//assertThat(importantLogic(), isNotNull()));
+		// mvc.perform(get("/"))
+		assertEquals(2 + 2, 4);
 	}
 
 }
